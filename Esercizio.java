11e33ed664474a3466e606d7e2b8c4fb1187c260
NicolasCaporale <nicolas.caporale@marconibari.edu.it>
2024-12-03 19:50:00 +0000

    gdfhogbfdygvfhudvb

--- Esercizio.java
+++ Esercizio.java
@@ -8,19 +8,57 @@ class Esercizio {
     // Il programma parte con una chiamata a main().
     public static void main(String args[])
     {
-        //Variabili del programma
-        String nome;
+              Scanner in = new Scanner(System.in);
+      Random rand = new Random();
+      
+      int n;
 
-        //Creo l'oggetto in per l'input da tastiera
-        Scanner in = new Scanner( System.in );
+      System.out.print("Inserire il numero di cifre da inserire in input: ");
+      n = Integer.parseInt(in.next());
+      
+      int[] u = new int[n];
+      
+      for(int i = 0; i < n; i++){
+        System.out.print("Inserire il " + (i+1) + " valore del vettore U: ");
+        u[i] = Integer.parseInt(in.next());
+        if(u[i] < 0){
+            do{
+                System.out.print("INserire un valore positivo: ");
+                u[i] = Integer.parseInt(in.next());
+            }while(u[i] < 0);
+        }
+      }
 
-        //Leggo l'input da tastiera
-        System.out.print("Inserisci il tuo nome: ");
-        nome = in.nextLine();
+      int[] r = new int[n];
+      for(int i = 0; i < n; i++){
+        r[i] = rand.nextInt(11);
+      }
 
-        //Output del nome acquisito da tastiera
-        System.out.println("Ciao "+nome+"!");
-    }
+      int[] s = new int[n];
+      for(int i = 0; i < n; i++){
+        s[i] = rand.nextInt(6) + 3;
+      } 
+
+      float[] m = new float[n];
+      for(int i = 0; i < n; i++){
+        m[i] = (u[i] + r[i] + s[i]) / 3;
+      }
+
+      for(int i = 0; i < n; i++){
+        System.out.println((i+1) + "° elemento di M: " + m[i]);
+      }
+
+      int t;
+      System.out.print("Inserire un numero: ");
+      t = Integer.parseInt(in.next());
+      for(int i = 0; i < n; i++){
+        for(int j = i+1; j < n; j++){
+          if(t == (u[i] + u[j])){
+            System.out.println(u[i] + " + " + u[j] + " = " + t);
+          }
+        }
+      }
+
+    
+  }
 }
-
-//LEGGERE LE ISTRUZIONI NEL FILE README.md
